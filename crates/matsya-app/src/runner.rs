b5a2e5e6e7f757a7bdd1cb384f7d9bf1290e1828
udsamani <uditpsamani@hotmail.com>
2024-10-06 01:24:08 +0100

    run the application

--- crates/matsya-app/src/runner.rs
+++ crates/matsya-app/src/runner.rs
@@ -1,6 +1,23 @@
+use config::Config;
 use matsya_common::MatsyaRelayResult;
 
 #[allow(dead_code)]
+#[async_trait::async_trait]
 pub trait Runner {
-    fn run(&self) -> MatsyaRelayResult<()>;
+    async fn run(&self) -> MatsyaRelayResult<()>;
+
+    fn config(&self) -> &Config;
+}
+
+#[allow(dead_code)]
+pub fn run_app<R: Runner>(runner: R) {
+    let config = runner.config();
+    let worker_threads = config.get_int("tokio_worker_threads").unwrap_or(4);
+
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(worker_threads as usize)
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async { runner.run().await.unwrap() })
 }
